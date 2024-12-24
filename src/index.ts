d2f799aedd5c58ecb9a9291d58d2f7ef733d51ae
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-24 13:12:52 -0800

    chore: add more exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 export { createMutation } from "./mutations";
-export { Err, Ok, Result, tryAsync, trySync } from "./result";
+export {
+	Err,
+	Ok,
+	InferErr,
+	InferOk,
+	Result,
+	tryAsync,
+	trySync,
+} from "./result";
 export { ServiceFn, createServiceErrorFns } from "./services";
