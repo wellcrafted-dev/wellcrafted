1d6da48ed6250881aac2e72daebe3f1b08770a85
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-12-24 22:44:12 -0800

    wip feat: runMutation

--- src/mutations.ts
+++ src/mutations.ts
@@ -4,8 +4,8 @@ import { Ok } from "./result";
 export function createMutation<
 	I,
 	O,
-	MutationFnError,
-	OnMutateError,
+	MutationFnError = never,
+	OnMutateError = never,
 	TContext = undefined,
 >({
 	mutationFn,
@@ -57,4 +57,57 @@ export function createMutation<
 		onSettled(result, { input, contextResult });
 	};
 	return mutate;
+}
+
+export function runMutation<
+	O,
+	MutationFnError = never,
+	OnMutateError = never,
+	TContext = undefined,
+>({
+	mutationFn,
+	onMutate = () => Ok(undefined as TContext),
+	onSuccess = () => undefined,
+	onError = () => undefined,
+	onSettled = () => undefined,
+}: {
+	mutationFn: (args: { context: TContext }) =>
+		| Promise<Result<O, MutationFnError>>
+		| Result<O, MutationFnError>;
+	onMutate?: () =>
+		| Promise<Result<TContext, OnMutateError>>
+		| Result<TContext, OnMutateError>;
+	onSuccess?: (output: O, args: { context: TContext }) => void;
+	onError?: (
+		error: MutationFnError | OnMutateError,
+		args: {
+			contextResult: Result<TContext, OnMutateError>;
+		},
+	) => void;
+	onSettled?: (
+		result: Result<O, MutationFnError | OnMutateError>,
+		args: { contextResult: Result<TContext, OnMutateError> },
+	) => void;
+}) {
+	const mutate = async (): Promise<void> => {
+		const contextResult = await onMutate();
+		if (!contextResult.ok) {
+			const error = contextResult.error;
+			onError(error, { contextResult });
+			onSettled(contextResult, { contextResult });
+			return;
+		}
+		const context = contextResult.data;
+		const result = await mutationFn({ context });
+		if (!result.ok) {
+			const error = result.error;
+			onError(error, { contextResult });
+			onSettled(result, { contextResult });
+			return;
+		}
+		const output = result.data;
+		onSuccess(output, { context });
+		onSettled(result, { contextResult });
+	};
+	return mutate();
 }
